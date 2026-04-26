51611f484376b474c7b16c5751df9815fd569a18
root <root@quirky-saha.74-208-50-211.plesk.page>
2026-04-26 12:56:39 +0000

    chore: remove dead Node.js/Express monorepo code

--- packages/queue/src/connection.ts
+++
@@ -1,7 +0,0 @@
-import { Redis } from "ioredis";
-
-const REDIS_URL = process.env.REDIS_URL || "redis://redis:6379";
-
-export const redisConnection = new Redis(REDIS_URL, {
-  maxRetriesPerRequest: null,
-});
